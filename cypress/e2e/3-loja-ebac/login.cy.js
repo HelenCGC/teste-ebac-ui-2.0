c173cdfead6747847a3e9044299d8c4ee942371d
Helen Carvalho <98592311+HelenCGC@users.noreply.github.com>
2024-10-22 15:22:11 +0100

    Massa de dados e Fixture

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,8 +1,9 @@
 ///<reference types="cypress" />;
+const perfil = require("../../fixtures/perfil.json");
 
 describe("Funcionalidade: Login", () => {
   beforeEach(() => {
-    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.visit("minha-conta");
   });
 
   //afterEach(() => {    cy.screenshot();  });
@@ -37,5 +38,28 @@ describe("Funcionalidade: Login", () => {
       "Erro: A senha fornecida para o e-mail helen.teste@teste.com está incorreta. Perdeu a senha?"
     );
     cy.get(".woocommerce-error").should("exist");
+  });
+
+  it("Deve fazer login com sucesso - Usando massa de dados", () => {
+    cy.get("#username").type(perfil.usuario);
+    cy.get("#password").type(perfil.senha);
+    cy.get(".woocommerce-form > .button").click();
+
+    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
+      "contain",
+      "Olá, helen.teste (não é helen.teste? Sair)"
+    );
+  });
+  it.only("Deve fazer login com sucesso - Usando Fixture", () => {
+    cy.fixture("perfil").then((dados) => {
+      cy.get("#username").type(dados.usuario, { log: false });
+      cy.get("#password").type(dados.senha, { log: false });
+      cy.get(".woocommerce-form > .button").click();
+
+      cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
+        "contain",
+        "Olá, helen.teste (não é helen.teste? Sair)"
+      );
+    });
   });
 });
