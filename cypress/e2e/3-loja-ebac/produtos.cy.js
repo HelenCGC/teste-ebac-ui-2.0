78d59a60922f223aaea4e73f75d2d8297d6961e6
Helen Carvalho <98592311+HelenCGC@users.noreply.github.com>
2024-10-22 17:49:34 +0100

    page-objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,23 @@
 ///<reference types="cypress" />;
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe("Funcionalidade: Produtos", () => {
   beforeEach(() => {
-    cy.visit("produtos");
+    produtosPage.visitarURL();
   });
 
   it("Deve selecionar um produto da lista", () => {
-    cy.get(".product-block")
-      //.first()
-      //.last()
-      //.eq(2)
-      .contains("Ariel Roll Sleeve Sweatshirt")
-      .click();
-
+    produtosPage.buscarProdutoLista("Atlas Fitness Tank");
     cy.get("#tab-title-description > a").should("contain", "Descrição");
   });
+
+  it.only("Deve buscar um produto com sucesso", () => {
+    let produto = "Apollo Running Short";
+    produtosPage.buscarProduto(produto);
+    cy.get(".product_title").should("contain", produto);
+  });
+
+  it("Deve visitar a página do produto", () => {});
+
+  it("Deve adicionar produto ao carrinho", () => {});
 });
